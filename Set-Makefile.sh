de2d85f25c6acb43bc0d4f47a032304161acc05a
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-13 18:15:09 -0300

    Tweaks in the `Set-Makefile` script

--- Set-Makefile.sh
+++ Set-Makefile.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# TODO Fazer ser possível substituir ou não os `Makefile` em cada pasta
-
 set -euo pipefail
 
-BMAKE="Base-Makefile"
-MAKE="Makefile"
-
 CURRENT_DIR="$(pwd)"
+
 SDIR="${CURRENT_DIR}/src"
 
+BMAKE="Base.mk"
+MAKE="Makefile"
+
 for dir in "${SDIR}"/*/; do
-  if [[ -d "${dir}" ]]; then
-    if [[ -f "${dir}/${MAKE}" ]]; then
-      cp -r "${BMAKE}" "${dir}/${MAKE}"
-    fi
+  dir=${dir%*/}
+  if [[ ! -f "${dir}/${MAKE}" ]]; then
+    echo "Copying ${BMAKE} to ${dir}/${MAKE}"
+    cp -r "${BMAKE}" "${dir}/${MAKE}"
   fi
 done
