ab685827a0494f1070dee104f936f972641f1cb2
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-13 18:15:31 -0300

    Still don't work :(

--- src/01/main.c
+++ src/01/main.c
@@ -13,16 +13,13 @@ void appendChar(char *str, char ch) {
 }
 
 int getNum(char string[]) {
-  int *nums;
-  int *pnum = &nums[0];
+  int num;
   char str_num[64] = "";
 
   printf("%s\n", str_num);
-  int i = 0;
   for (char *ch = &string[0]; *ch != '\0'; ch++) {
     if (isdigit(*ch)) {
-      nums[i] = appendChar(str_num, *ch);
-      i++;
+      appendChar(str_num, *ch);
     }
   }
   toInt(str_num, &num);
@@ -32,10 +29,11 @@ int getNum(char string[]) {
 int main(void) {
   long int sum = 0;
   char text[1024];
-  FILE *pfile = fopen("input.txt", "r");
+  FILE *pfile = fopen("t-input.txt", "r");
   while (fgets(text, 1024, pfile)) {
-    sum += getNum(text);
+    sum = getNum(text);
   }
+  // Expect: 84
   printf("sum = %li\n", sum);
   fclose(pfile);
   return 0;
